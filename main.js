d31ceab8f15cc9a0e8ca46fa6bfbd5360a13d29d
Prithvi2011 <76046770+Prithvi2011@users.noreply.github.com>
2022-06-27 17:17:08 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ leftWristX = 0;
 leftWristY = 0;
 
 function preload(){
-    song1 = loadSong(music.mp3);
-    song2 = loadSong(music2.mp3);
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
 }
 function setup(){
     canvas = createCanvas(600,500,);
@@ -48,5 +48,40 @@ function gotPoses(results){
      }
 }
 function draw(){
-    image(video ,0 ,0 ,500 ,600)
+    image(video ,0 ,0 ,600 ,500)
+
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+
+    fill("#FF0000");
+    stroke("#FF0000")
+
+    if(scoreRightWrist > 0.2){
+
+        circle(rightWristX,rightWristY,20)
+        song2.stop();
+
+        if(song1_status == false){
+            song1.play()
+
+            document.getElementById("song").innerHTML = "Playing - Tom & Jerry song"
+        }
+    }
+
+    if(scoreLeftWrist > 0.2){
+        circle(leftWristX,leftWristY,20)
+        song1.stop();
+
+        if(song2_status == false ){
+            song2.play()
+
+            document.getElementById("song").innerHTML = " Playing - calm music"
+        }
+
+    }
+}
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
